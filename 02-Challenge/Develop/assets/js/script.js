5c3908e3ef0a13b936de072a577ca104a919183b
Mariana Bernal <marianabmartinez22@gmail.com>
2024-06-02 23:07:38 -0600

    Implemented function to add task-card into the To Do box. Made task-card draggable in box

--- 02-Challenge/Develop/assets/js/script.js
+++ 02-Challenge/Develop/assets/js/script.js
@@ -1,6 +1,53 @@
-//Todo: Create a function that generates a unique task id
-function generateTask() {
+const taskInput = $('#task');
+const descriptionInput = $('#description');
+const datePickerInput = $('#datepicker');
+const taskForm = $('#task-form');
+const todoBox = $('#todo-cards');
 
+// Creates an empty task list 
+const taskList = [];
+
+$( "#dialog" ).dialog({
+    autoOpen: false,
+    minHeight: 400,
+    minWidth: 500,
+  });
+
+  datePickerInput.datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+
+
+function handleFormSubmit(event){
+  event.preventDefault()
+
+    // Create a task-ID
+    const taskId = generateTask();
+    
+    // create task object using form values
+    const task = {
+        id: taskId,
+        name: taskInput.val(),
+        date: datePickerInput.val(),
+        description: descriptionInput.val()
+    }
+
+    const div = createTaskCard(task);
+  
+   todoBox.append(div);   
+  $(`#task-${taskId}`) .draggable({ grid: [ 20, 20 ] })
+
+    $('#dialog').dialog('close');
+   // Should clear form 
+   // todo: checkk why the form does not clear 
+   // $('#dialog').find('form').reset();
+}
+
+$('#dialog').find('form').on('submit', handleFormSubmit);
+
+// Generates a task ID
+function generateTask() {
     if(localStorage.getItem('nextId') === null){
         localStorage.setItem('nextId', 1);
         return 0;
@@ -13,7 +60,37 @@ function generateTask() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+//const div =  $(`
+// <div id="task-${task.id}" class="task-card">
+//  <div class="task-title card-divs">
+//    ${task.name}
+//    <br>
+//  </div>
+//  <div class="task-content card-div">
+//    ${task.date}
+//    </br>
+//    ${task.description}
+//    <br>
+//  </div>
+//  <div class="card-divs">
+//  <button> DELETE </button>
+//  </div>
+// </div>
+//`)
+const div = $(`
 
+<div id="task-${task.id}" class=" card">
+  <div class="card-body">
+    <h5 class="card-title">${task.name}</h5>
+    <p class="card-text">${task.date}</p>
+    <p class="card-text">${task.description}</p>
+    <button class="btn btn-primary"> DELETE </button>
+  </div>
+</div>
+
+`)
+
+return div;
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -23,7 +100,14 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    const taskId = generateTask();
+    $( "#dialog" ).dialog( "open" );
+ 
+    $( "#datepicker" ).datepicker({
+     changeMonth: true,
+     changeYear: true
+   });
+ 
 }
 
 // Todo: create a function to handle deleting a task
@@ -40,3 +124,4 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
